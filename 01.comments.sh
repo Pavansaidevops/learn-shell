ae6dc2061c7dc6d75d8f142f9f0b9e8c008ffb3e
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-02 11:59:00 +0530

    identifying Catalogue re-installation error

---
+++ 01.comments.sh
@@ -0,0 +1,2 @@
+# if we use # starting of the line then it is considered as a comment
+echo Hello World
---
+++ 02.print.sh
@@ -0,0 +1,22 @@
+# echo command prints the output
+echo hello world
+# printing colours in linux
+# red 31
+# green 32
+# yellow 33
+# blue 34
+# magenta 35
+# cyan 36
+
+# Syntax
+# echo -e "\e[colourcodemMessage\e[0m"
+
+# here colour code reprents the code of the colour and message reprents the content and tha last 0m resets the colour
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
+echo -e "\e[37mHello World\e[0m"
