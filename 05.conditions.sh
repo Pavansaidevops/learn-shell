b931a973794d88b1e7e5660f2a3daebf032ccbc0
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-03 17:58:23 +0530

    shell scripting

---
+++ 05.conditions.sh
@@ -0,0 +1,21 @@
+# Number expressions
+ # -eq equal to
+ # -ne not equal to
+ # -lt less than
+ # -le less than or equal to
+ # -gt greater than
+ # -ge greater than or equal to
+
+
+# String expressions
+  # == equal to
+  ##!= not equal to
+  # -z check variable is empty or not
+
+# File expressions
+  # -e /opt/file
+a=10
+
+ if [ $a gt $0 ] ; then
+  echo $a is greater than zero
+ fi
